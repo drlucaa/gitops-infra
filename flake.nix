1f17340ba88e4f47bcb5a1b677c25522cf0483a3
Luca Fondo <luca.fondo@trai.ch>
2026-02-15 16:16:40 +0100

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Development environment for GitOps";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            kubectl
+            kubectx
+
+            opentofu
+            fluxcd
+
+            minio-client
+
+            sops
+            age
+
+            ansible
+            sshpass
+
+            nixfmt-tree
+            nixfmt
+            nixd
+          ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
