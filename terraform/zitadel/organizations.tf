d592384350942d070ab70b3bc46324517fc986b9
Luca Fondo <luca.fondo@trai.ch>
2026-02-23 23:24:28 +0100

    Resolve circular dependency in zitadel terraform config

--- terraform/zitadel/organizations.tf
+++ terraform/zitadel/organizations.tf
@@ -1,12 +1,10 @@
 resource "zitadel_org" "home" {
   name       = "home"
   is_default = true
-  admins = [
-    {
-      user_id = zitadel_human_user.admin_user.id
-      roles   = ["ORG_OWNER"]
-    }
-  ]
-
 }
 
+resource "zitadel_org_member" "admin_user_member" {
+  org_id  = zitadel_org.home.id
+  user_id = zitadel_human_user.admin_user.id
+  roles   = ["ORG_OWNER"]
+}
